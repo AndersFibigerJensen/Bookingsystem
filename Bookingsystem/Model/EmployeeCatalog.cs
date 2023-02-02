6a49e63c347907e67958af04f9943f9289814fc3
juliefroeshoej <julf@itu.dk>
2023-02-02 11:27:12 +0100

    bnnv

---
+++ Bookingsystem/Model/EmployeeCatalog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookingsystem.Model
+{
+    public class EmployeeCatalog
+    {
+        private List<Employee> _employees;
+
+       public List<Employee> Employees
+        { get { return _employees; } set { _employees = value; } }
+
+        public EmployeeCatalog()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee newEmployee) 
+        {
+            _employees.Add(newEmployee);
+        }
+
+        public void PrintAll ()
+        {
+            foreach (Employee e in _employees)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public Employee GetByPhoneId (string phoneId)
+        {
+            foreach (Employee e in _employees)
+            {
+                if(e.Phone==phoneId)
+                    return e;
+            }
+            return null;
+        }
+
+
+    }
+
+
+}
