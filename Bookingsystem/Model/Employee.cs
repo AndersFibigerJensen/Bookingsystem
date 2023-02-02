924da4c812f44171bcda02a0fa34d79b67c6a7bb
Mammon22 <anders99fib@gmail.com>
2023-02-02 10:56:51 +0100

    Add project files.

---
+++ Bookingsystem/Model/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookingsystem.Model
+{
+    public class Employee
+    {
+		private string _phone;
+        private string _name;
+        private string _frisør;
+
+        public string Phone
+		{
+			get { return _phone; }
+			set { _phone = value; }
+		}
+
+		
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		
+
+		public string Frisør
+		{
+			get { return _frisør; }
+			set { _frisør = value; }
+		}
+
+		public Employee()
+		{
+
+		}
+
+		public Employee(string Phone,string Name, string Frisør)
+		{
+			_phone= Phone;
+			_name= Name;
+			_frisør = Frisør;
+
+		}
+
+        public override string ToString()
+        {
+            return $" Phone number: {_phone}  Name: {_name}  Frisør: {_frisør}";   
+        }
+
+    }
+}
---
+++ Bookingsystem/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bookingsystem.Model;
+
+Employee Julie = new Employee("59595959", "Julie", "elev");
+Employee Tor = new Employee("69696969696", "Tor", "Frisør");
+Employee Sarah = new Employee("8888888888", "Sarah´", "Frisør");
+Console.WriteLine(Julie.ToString());
+Console.WriteLine(Tor.ToString());
+Console.WriteLine(Sarah.ToString());
+
